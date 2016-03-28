7eb48093b53e1d894f0d994418b9ea6bb6a7d304
Tatiana Ryzhkova <tatvasilna@gmail.com>
2016-03-28 10:52:58 -0700

    First commit

---
+++ Junit/src/main/java/core/PrNuCheck.java
@@ -0,0 +1,10 @@
+package core;
+
+public class PrNuCheck {
+       public Boolean validate(final Integer primeNumber) {
+              for (int i = 2; i < (primeNumber / 2); i++) {
+                     if (primeNumber % i == 0) {
+                        return false;
+                     }  }
+
+              return true;    }  }
